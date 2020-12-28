39a734b4a5f6c84ad82ddb1e80c694148df9573c
yangshijay <72003601+yangshijay@users.noreply.github.com>
2020-12-28 14:40:54 +0800

    Add files via upload

---
+++ 黃政傑-week16.py
@@ -0,0 +1,38 @@
+print(True)
+print(False)
+
+print(bool(1))
+print(bool(0))
+print(bool(()))
+
+print(False + 1)
+
+score = 66
+score == 66
+
+name = "Francis"
+name == "Francis"
+name == "francis"
+
+5>2
+5 <2
+5 !=2
+
+True & True
+True & False
+False & True
+False & False
+True and False
+True or False
+False or True
+False| False
+not True
+not False
+
+X = 70
+print((X > 60) and (X < 80))
+print((X > 60) or (X < 80))
+print(not(X > 60))
+
+"H" in "Hello world"
+"h" in "Hello world"
